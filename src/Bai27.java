77f33ed463f480d2b531938648256d52dbd1e1f1
duchuyvo <voduchuy016688@gmail.com>
2022-12-31 16:47:03 +0700

    bai27

--- src/Bai27.java
+++ src/Bai27.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Bai27 {
+    private static long tinh(int n) {
+        int p = 1;
+        for (int i = 0; i <= n; i++) {
+            p = p * (2 * i + 1);
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("nhap so n:");
+        int n=scanner.nextInt();
+        System.out.println("gia tri cua (2n+1) la:"+tinh(n));
+    }
 }
