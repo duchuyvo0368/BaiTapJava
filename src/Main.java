02c04c4d84dfa6bc5247c62d84b13831d5661bab
duchuyvo <voduchuy016688@gmail.com>
2023-02-09 08:39:02 +0700

    fixdong10,33

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,7 @@ public class Main {
 
         int sum = 0;
         int primeCount = 0;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             if (i % 2 == 0) {
                 sum += i;
             }
